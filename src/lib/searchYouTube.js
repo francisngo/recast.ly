c587872e9e1d4a0e3f6984a0310497dd8571bb27
francisngo <francis.t.ngo@gmail.com>
2017-07-20 12:25:03 -0700

    create ajax call to YouTube API

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,51 @@
-var searchYouTube = (options, callback) => {
+var searchYouTube = ({key = window.YOUTUBE_API_KEY, query, max = 5}, callback) => {
   // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {
+      part: 'snippet',
+      key: key,
+      q: query,
+      maxResults: max,
+      type: 'video',
+    },
+    success: function(data) {
+      // console.log(data);
+      // console.log('success ran');
+      callback(data.items);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
+
+
+/////////////////////////
+
+// var Videos = Backbone.Collection.extend({
+
+//   model: Video,
+
+//   url: 'https://www.googleapis.com/youtube/v3/search',
+
+//   search: function(query) {
+
+//     this.fetch({
+//       data: {
+//         part: 'snippet',
+//         key: window.YOUTUBE_API_KEY,
+//         q: query,
+//         maxResults: 5,
+//         type: 'video',
+//         videoEmbeddable: true,
+//       }
+//     });
+
+//   },
+
+//   parse: function(response) {
+//     return response.items;
+//   }
+
+// });
