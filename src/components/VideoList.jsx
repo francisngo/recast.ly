2b1120c6bc6af29cd269c214794e01e067d22a2b
francisngo <francis.t.ngo@gmail.com>
2017-07-20 08:32:10 -0700

    render videolist title, description, and thumbnail dynamically`

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,21 +1,17 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list media">
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
+    {props.videos.map((video, index) => {
+      return (
+        <VideoListEntry key={index} video={video} />
+      );
+    })}
   </div>
 );
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
-  videos: React.PropTypes.array.isRequired
+  videos: React.PropTypes.array.isRequired,
 };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
